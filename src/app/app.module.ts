8d66d3839234c2af40297b5ba42d19c6bcc9aaa5
Lucky Prabath <lucky.prabath94@gmail.com>
2021-01-03 16:16:17 +0530

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
 
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -46,8 +47,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule
   ],
-  providers: 
-  [DishService
+  providers: [
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,9 +10,9 @@ import { DishService } from '../services/dish.service';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[];
+  dishes! : Dish[];
 
-  selectedDish: Dish;
+  selectedDish!: Dish;
 
   constructor(private dishService: DishService) {}
 
