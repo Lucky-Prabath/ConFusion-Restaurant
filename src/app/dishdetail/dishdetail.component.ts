eb85b15d1026e8ae1143e40c7a1fedc9ece193c4
Lucky Prabath <lucky.prabath94@gmail.com>
2021-01-07 23:27:49 +0530

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,7 +20,8 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    this.dishService.getDish(id)
+    .then((dish) => (this.dish = dish));
   }
 
   goBack(): void{
